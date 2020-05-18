9a2df6eaf896dbb28f4aa024bb1335419f41d646
kazhurka <42044750+kazhurka@users.noreply.github.com>
2020-05-18 15:50:52 +0300

    Вносит правки, добавляет блок "абонементы".

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,43 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var buttonsListEl = document.querySelectorAll('.membership__button');
 
-pageHeader.classList.remove('page-header--nojs');
+var picsList = document.querySelector('.membership__offers ul');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+
+var position = 0;
+
+var widthPic = 400;
+var countPic = 3;
+
+var getParam = function () {
+  if (window.screen.width < 1200 && window.screen.width > 767) {
+    widthPic = 488;
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    if (window.screen.width < 768) {
+      widthPic = 419;
+    } else {
+      widthPic = 400;
+    }
   }
+};
+
+
+getParam();
+buttonsListEl.forEach(function (item) {
+
+  item.onclick = function (evt) {
+    event.preventDefault();
+    var activePage = document.querySelector('.membership__button--active');
+    activePage.classList.remove('membership__button--active');
+    var buttonsListArr = Array.from(buttonsListEl);
+    position = -buttonsListArr.indexOf(evt.target.parentNode) * (widthPic * countPic);
+    if (screen.width > 1200) {
+      picsList.style.marginLeft = position + 'px';
+      evt.target.parentNode.classList.add('membership__button--active');
+    } else {
+      picsList.style.marginTop = position + 'px';
+      evt.target.parentNode.classList.add('membership__button--active');
+    }
+
+  };
 });
