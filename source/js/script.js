5e5d38f9d35b00b1582bef8571aee46c5db2d4c1
kazhurka <42044750+kazhurka@users.noreply.github.com>
2020-06-19 18:35:41 +0300

    Исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,47 +1,59 @@
 'use strict';
-var buttonsListEl = document.querySelectorAll('.membership__button');
-
-var picsList = document.querySelector('.membership__offers ul');
 
 
-var position = 0;
-
-var widthPic = 400;
-var countPic = 3;
-
-var getParam = function () {
-  if (window.screen.width < 1200 && window.screen.width > 767) {
-    widthPic = 488;
-  } else {
-    if (window.screen.width < 768) {
-      widthPic = 419;
-    } else {
-      widthPic = 400;
-    }
-  }
+var membershipLink = document.querySelector('#to-membership');
+membershipLink.onclick = function (evt) {
+  evt.preventDefault();
+  window.scroll({
+    top: 1400,
+    left: 0,
+    behavior: 'smooth'
+  });
 };
 
+(function () {
+  var buttonsListEl = document.querySelectorAll('.membership__button');
+  var picsList = document.querySelector('.membership__offers ul');
 
-getParam();
-buttonsListEl.forEach(function (item) {
 
-  item.onclick = function (evt) {
-    event.preventDefault();
-    var activePage = document.querySelector('.membership__button--active');
-    activePage.classList.remove('membership__button--active');
-    var buttonsListArr = Array.from(buttonsListEl);
-    position = -buttonsListArr.indexOf(evt.target.parentNode) * (widthPic * countPic);
-    if (screen.width > 1200) {
-      picsList.style.marginLeft = position + 'px';
-      evt.target.parentNode.classList.add('membership__button--active');
+  var position = 0;
+
+  var widthPic = 400;
+  var countPic = 3;
+
+  var getParam = function () {
+    if (window.screen.width < 1200 && window.screen.width > 767) {
+      widthPic = 488;
     } else {
-      picsList.style.marginTop = position + 'px';
-      evt.target.parentNode.classList.add('membership__button--active');
+      if (window.screen.width < 768) {
+        widthPic = 419;
+      } else {
+        widthPic = 400;
+      }
     }
-
   };
-});
 
+
+  getParam();
+  buttonsListEl.forEach(function (item) {
+
+    item.onclick = function (evt) {
+      event.preventDefault();
+      var activePage = document.querySelector('.membership__button--active');
+      activePage.classList.remove('membership__button--active');
+      var buttonsListArr = Array.from(buttonsListEl);
+      position = -buttonsListArr.indexOf(evt.target.parentNode) * (widthPic * countPic);
+      if (screen.width > 1200) {
+        picsList.style.marginLeft = position + 'px';
+        evt.target.parentNode.classList.add('membership__button--active');
+      } else {
+        picsList.style.marginTop = position + 'px';
+        evt.target.parentNode.classList.add('membership__button--active');
+      }
+
+    };
+  });
+})();
 // trainers-slider
 
 (function () {
